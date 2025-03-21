637825ed684ede2248479fdb57f59984174a4034
Mike3247 <Mike3247@github.com>
2025-03-21 18:33:48 -0400

    routing experiments

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -4,6 +4,18 @@ const ErrorPage = () => {
   return (
     <div>
       <h1>Oh no, this route doesn't exist!</h1>
+      <h2>You are invited to choose a profile to continue</h2>
+      <form action="">
+        <input type="text"/>
+      </form>
+      <Link to="profile/popeye">
+        Popeye's Profile
+      </Link>
+      <br />
+      <Link to="profile/spinach">
+        Spinach's Profile
+      </Link>
+      <br />
       <Link to="/">
         You can go back to the home page by clicking here, though!
       </Link>
